51dc7402df90d356ee4f5308160ded19be33fe8d
Bonflintstone <freddy@railslove.com>
2018-12-03 14:23:44 +0700

    Solve both tasks in 1, and improve things

--- 2018/day3/claims3.nim
+++ 2018/day3/claims3.nim
@@ -2,28 +2,38 @@
 
 import re, rdstdin, strutils, sequtils
 
-var lines = newSeq[string]()
-var line: TaintedString
-while readlineFromStdin("", line):
-  lines.add(line)
+func present(s: string): bool = len(s) > 0
+let lines = stdin.readAll().splitLines().filter(present)
 
 var fabric: array[1000, array[1000, int]]
 
 iterator eachCell(claim: string): (int, int) =
   let nums = claim.findAll(re(r"\d+")).map(parseInt)
 
-  for x in nums[1]..nums[1]+nums[3]:
-    for y in nums[2]..nums[2]+nums[4]:
+  for x in nums[1]..<nums[1]+nums[3]:
+    for y in nums[2]..<nums[2]+nums[4]:
       yield (x, y)
 
-for claim in lines:
-  for x, y in eachCell(claim):
-    fabric[x][y] += 1
-
-for claim in lines:
-  block checkClaim:
+proc prepare() =
+  for claim in lines:
     for x, y in eachCell(claim):
-      if fabric[x][y] != 1: break checkClaim
+      inc fabric[x][y]
 
-    echo claim
-    break
+proc task1(): int =
+  var overlapCount = 0
+  for row in fabric:
+    for cell in row:
+      if cell > 1: inc result
+
+proc task2(): string =
+  for claim in lines:
+    block checkClaim:
+      for x, y in eachCell(claim):
+        if fabric[x][y] != 1: break checkClaim
+
+      return claim
+
+prepare()
+
+echo task1()
+echo task2()
